48d4f7f1f492e41f6eda36754544e3ebad579bc3
Todor Grudev <tagrudev@gmail.com>
2011-10-12 14:02:41 +0300

    Adding params

--- spec/memo_rage/tvshow_spec.rb
+++ spec/memo_rage/tvshow_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe MemoRage::TvShow, '.find' do
   context 'with valid tconst given' do
     it 'should get main details' do
-      mock_api :get, 'title/maindetails', 'title/maindetails-buffy', :params => { :tconst => 'buffy' } do
+      mock_api :get, 'title/maindetails', 'title/maindetails-buffy', :params => { :tconst => 'buffy', :title => 'Buffy', :year => '2011' } do
         movie = MemoRage::TvShow.find('buffy')
         movie.tconst.should == "buffy"
         movie.title.should == "Buffy"
